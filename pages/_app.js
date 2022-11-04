4e8103470871cd89dcdb3ed0f33cf9a2bb52b20b
Brandon F. Creed <brandon.f.creed@gmail.com>
2022-11-04 15:27:54 -0400

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // You can add here any thing that will appear on
+  // every page of your application like Navbar or Footer
+  // This is essentially the layout page
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const about = () => {
+  return (
+    <div>
+        <Head>
+            <title>About</title>
+        </Head>
+        About
+    </div>
+  )
+}
+
+export default about
